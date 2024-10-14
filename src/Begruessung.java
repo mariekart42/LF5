d5ec3191424574f9a5f2aa8a8c8fd9e247336660
Marie Mensing <marie.mensing@mention.de>
2024-10-14 19:13:21 +0200

    init

---
+++ src/Begruessung.java
@@ -0,0 +1,12 @@
+public class Begruessung
+{
+    public static void main(String[] args)
+    {
+        //System.out.printf("Hallo Programmierer!\nProgrammieren mit\nJava macht Spaß.");
+        String[] lol = {"Hallo Programmierer!", "Programmieren mit", "Java macht Spaß."};
+        for (int i = 0; i < lol.length; i++)
+        {
+            System.out.println(lol[i]);
+        }
+    }
+}
---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class HelloWorld {
+    public static void main(String[] args) {
+        /* Beispiel Zeile 1
+        Beispiel Zeile 2 */
+        System.out.println("Hello World");
+    }
+}
